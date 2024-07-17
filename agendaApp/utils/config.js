5d87e1778d6729246c7a18af16192b43dfcec6f9
Agus Bussi <agusbuu.dev@gmail.com>
2024-07-17 16:21:43 -0500

    ejercicios 4.8 a 4.12

--- agendaApp/utils/config.js
+++ agendaApp/utils/config.js
@@ -4,8 +4,12 @@ const PORT = process.env.PORT
 const user= process.env.DB_MONGO_USER
 const password = process.env.DB_MONGO_PASS
 const uri = process.env.DB_MONGO_URI
+const uri_test=process.env.DB_MONGO_URI_TEST
 
-const MONGO_URI =`mongodb+srv://${user}:${password}${uri}`
+const MONGO_URI =process.env.NODE_ENV === 'test' 
+? `mongodb+srv://${user}:${password}${uri}`
+: `mongodb+srv://${user}:${password}${uri_test}`
+
 module.exports = {
 
   MONGO_URI,
